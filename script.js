0e333d5a13e2c31b9d07febed665798f63afae74
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-04-14 17:26:52 +0200

    Coding...

--- script.js
+++ script.js
@@ -2,16 +2,41 @@ function fetchUser() {
   fetch('https://randomuser.me/api/')
     .then((res) => res.json())
     .then((data) => {
+      console.log(data.results[0]);
       displayUser(data.results[0]);
     });
 }
 
 function displayUser(user) {
+  const userDisplay = document.querySelector('#user');
+
+  console.log(user.gender);
   if (user.gender === 'female') {
-    document.body.style.backgroundColor = 'pink';
+    document.body.style.backgroundColor = 'purple';
   } else {
     document.body.style.backgroundColor = 'lightblue';
   }
+  userDisplay.innerHTML = `     <div class="flex justify-between">
+        <div class="flex">
+          <img class="w-48 h-48 rounded-full mr-8" src="${user.picture.large}" />
+          <div class="space-y-3">
+            <p class="text-xl">
+              <span class="font-bold">Name: </span>${user.name.first} ${user.name.last} </p>
+            <p class="text-xl">
+              <span class="font-bold">Email: </span>${user.email}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Phone: </span>${user.phone}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Location: </span> ${user.location.city} ${user.location.country}
+            </p>
+            <p class="text-xl"><span class="font-bold">Age: </span>${user.dob.age}</p>
+          </div>
+        </div>
+      </div>`;
 }
+
+document.querySelector('#generate').addEventListener('click', fetchUser);
 
 fetchUser();
